bc93e85ac80707acfa386500a9cd874964fdf831
Oleksandr-Berd <alex.berd86@gmail.com>
2024-08-16 09:18:13 +0300

    made global styles themes and layouts

---
+++ my-react-app/src/styled/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+    color:{
+        white:"#fff",
+        pink:"#ee8b8b",
+        brown:"#ce9898",
+        black:"#423a3a",
+        grad:"#f8bfbf #ee8b8b linier"
+    }
+}
